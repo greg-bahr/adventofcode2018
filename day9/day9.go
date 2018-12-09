8f7abd0c7082bf7bdb273585339ef24a5f7c94d2
Greg Bahr <gregsbahr@gmail.com>
2018-12-09 14:01:20 -0600

    day 9

---
+++ day9/day9.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	file, err := ioutil.ReadFile("./day9/input.txt")
+	check(err)
+
+	re := regexp.MustCompile("\\d+")
+	nums := re.FindAllString(string(file), -1)
+
+	numPlayers, _ := strconv.Atoi(nums[0])
+	lastWorth, _ := strconv.Atoi(nums[1])
+
+	marbleValue := 1
+	currentPlayer := 0
+	marbles := ring.New(1)
+	players := make([]int, numPlayers)
+	marbles.Value = 0
+
+	for marbleValue <= lastWorth {
+		if marbleValue%23 == 0 {
+			players[currentPlayer] += marbleValue
+
+			for i := 0; i < 8; i++ {
+				marbles = marbles.Prev()
+			}
+			players[currentPlayer] += marbles.Next().Value.(int)
+			marbles.Unlink(1)
+			marbles = marbles.Next()
+		} else {
+			marbles = marbles.Next()
+			marbles.Link(&ring.Ring{Value: marbleValue})
+			marbles = marbles.Next()
+		}
+
+		marbleValue++
+		currentPlayer = (currentPlayer + 1) % numPlayers
+	}
+
+	maxScore := 0
+	for _, score := range players {
+		if score > maxScore {
+			maxScore = score
+		}
+	}
+	fmt.Println(maxScore)
+}
